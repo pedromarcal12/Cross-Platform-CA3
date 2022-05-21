ae0569cf135c9b3c7ce2a786b7bff36b0e030981
pedromarcal12 <henrique.pedro.marcal@gmail.com>
2022-05-21 23:09:01 +0100

    last updates  - done

--- eletric-clacl/index.js
+++ eletric-clacl/index.js
@@ -1,14 +1,15 @@
+ //To create electron extension.
 const electron = require("electron");
 const fs = require("fs");
 const uuid = require("uuid");
 
 const { app, BrowserWindow, Menu, ipcMain, tray } = electron;
 
+//Creating variables
 let frontPageWindow;
 let createWindow;
 let listWindow;
 
-let allResults = [];
 
 fs.readFile("db.json", (err, jsonAppointments) => {
   if (!err) {
@@ -22,6 +23,7 @@ app.on("ready", () => {
     webPreferences: {
       nodeIntegration: true
     },
+    //Main pagecode
     title: "Electricy Bill App"
   });
   frontPageWindow.loadURL(`file://${__dirname}/frontPage.html`);
@@ -37,6 +39,7 @@ app.on("ready", () => {
   Menu.setApplicationMenu(mainMenu);
 });
 
+//Creating calculator window
 const createWindowCreator = () => {
   createWindow = new BrowserWindow({
     webPreferences: {
@@ -47,6 +50,7 @@ const createWindowCreator = () => {
     title: "Calculator"
   });
 
+  //Creating windows 
   createWindow.setMenu(null);
 
   createWindow.loadURL(`file://${__dirname}/calculator.html`);
@@ -54,22 +58,6 @@ const createWindowCreator = () => {
   createWindow.on("closed", () => (createWindow = null));
 };
 
-const listWindowCreator = () => {
-  listWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true
-    },
-    width: 600,
-    height: 400,
-    title: "All Results"
-  });
-
-  listWindow.setMenu(null);
-
-  listWindow.loadURL(`file://${__dirname}/list.html`);
-
-  listWindow.on("closed", () => (listWindow = null));
-};
 
 ipcMain.on("appointment:create", (event, appointment) => {
   appointment["id"] = uuid();
@@ -109,6 +97,7 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
 const menuTemplate = [
   {
     label: "File",
@@ -121,14 +110,6 @@ const menuTemplate = [
           createWindowCreator();
 
 
-        }
-      },
-
-      {
-        label: "All bills",
-
-        click() {
-          listWindowCreator();
         }
       },
 
